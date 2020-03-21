90aa8e40c47b52c84e067c008d6bd7d307c28944
Emily A. Bellows <emily.a.bellows@gmail.com>
2020-03-21 18:03:58 -0400

    deleted unused code

--- fftbg/config.py
+++ fftbg/config.py
@@ -12,12 +12,3 @@ ALL_COLUMNS_PATH = DATA_PATH / 'all_columns.pickle'
 BOT_MEMORY_PATH = DATA_PATH / 'bot_memory.db'
 
 SAVE_MODEL = True
-
-# FIXME: Wrecking old style logging
-# LOGGING_CONFIG = toml.loads((CONFIG_PATH / 'logging.toml').read_text())
-# logging.config.dictConfig(LOGGING_CONFIG)
-#
-# BOT_CONFIG = toml.loads((CONFIG_PATH / 'twitch.toml').read_text())
-#
-# LOG = logging.getLogger(__name__)
-# LOG.info("Good morning")
