ddae600fafe0db2366db9a567e820b0a9deb2c6b
Emily A. Bellows <emily.a.bellows@gmail.com>
2020-04-10 00:05:32 -0400

    added some sword skills
    total: 15384
    correct: 63.4%
    time_outs: 0.6%
    improvement: 26.7%
    log loss: 0.936538

--- simulator/src/sim/actions/battle_skill.rs
+++ simulator/src/sim/actions/battle_skill.rs
@@ -1,8 +1,10 @@
 use crate::dto::rust::Equipment;
 use crate::sim::actions::attack::do_single_weapon_attack;
-use crate::sim::actions::common::mod_3_formula_xa;
+use crate::sim::actions::common::{mod_2_formula_xa, mod_3_formula_xa};
 use crate::sim::actions::{Ability, AbilityImpl, Action, ALLY_OK, FOE_OK, TARGET_NOT_SELF};
-use crate::sim::{Combatant, CombatantId, Condition, EquipSlot, Event, Simulation, Source};
+use crate::sim::{
+    Combatant, CombatantId, Condition, EquipSlot, Event, Simulation, Source, WeaponType,
+};
 
 pub const BATTLE_SKILL_ABILITIES: &[Ability] = &[
     // Head Break: weapon range, 0 AoE. Hit: (PA + WP + 45)%. Effect: Break target's head equipment; If none, attack instead.
@@ -50,10 +52,118 @@ pub const BATTLE_SKILL_ABILITIES: &[Ability] = &[
         },
     },
     // Magic Break: weapon range, 0 AoE. Hit: (PA + 50)%. Effect: DamageMP (50)%.
+    Ability {
+        name: "Magic Break",
+        flags: FOE_OK,
+        mp_cost: 0,
+        aoe: None,
+        implementation: &MagicBreakImpl {
+            base_chance: 50,
+            mp_percent: 0.5,
+        },
+    },
     // Speed Break: weapon range, 0 AoE. Hit: (PA + 50)%. Effect: -2 Speed.
     // Power Break: weapon range, 0 AoE. Hit: (PA + 50)%. Effect: -3 PA.
     // Mind Break: weapon range, 0 AoE. Hit: (PA + 50)%. Effect: -3 MA.
+
+    // Stasis Sword: 1 range, 1 AoE, 2 CT, 20 MP. Effect: Damage (PA * (WP + 1)); Chance to Add Stop.
+    Ability {
+        name: "Stasis Sword",
+        flags: FOE_OK,
+        mp_cost: 20,
+        aoe: Some(1),
+        implementation: &ChanceToAddSwordImpl {
+            wp_plus: 1,
+            chance_to_add: Condition::Stop,
+            range: 1,
+            ctr: 2,
+        },
+    },
+    // Justice Sword: 2 range, 0 AoE, 2 CT, 22 MP. Effect: Damage (PA * (WP + 2)); Chance to Add Death Sentence.
+    Ability {
+        name: "Justice Sword",
+        flags: FOE_OK,
+        mp_cost: 22,
+        aoe: Some(1),
+        implementation: &ChanceToAddSwordImpl {
+            wp_plus: 2,
+            chance_to_add: Condition::DeathSentence,
+            range: 2,
+            ctr: 2,
+        },
+    },
+    // Surging Sword: 1 range, 1 AoE, 3 CT, 24 MP. Effect: Damage (PA * (WP + 2)); Chance to Add Silence.
+    Ability {
+        name: "Surging Sword",
+        flags: FOE_OK,
+        mp_cost: 24,
+        aoe: Some(1),
+        implementation: &ChanceToAddSwordImpl {
+            wp_plus: 2,
+            chance_to_add: Condition::Silence,
+            range: 1,
+            ctr: 3,
+        },
+    },
+    // Explosion Sword: 4 range, 4 AoE (line), 2 CT, 26 MP. Effect: Damage (PA * (WP + 3)); Chance to Add Confusion.
+    // Shellburst Stab: 2 range, 0 AoE, 15 MP. Effect: Break target's body equipment; If successful Damage (PA * WP).
+    // Dark Sword: 2 range, 0 AoE, 2 CT, 18 MP. Effect: AbsorbMP (PA * WP).
+    // Night Sword: 2 range, 0 AoE, 3 CT, 22 MP. Effect: AbsorbHP (PA * WP).
 ];
+
+struct ChanceToAddSwordImpl {
+    wp_plus: i16,
+    chance_to_add: Condition,
+    range: i8,
+    ctr: u8,
+}
+
+impl AbilityImpl for ChanceToAddSwordImpl {
+    fn consider<'a>(
+        &self,
+        actions: &mut Vec<Action<'a>>,
+        ability: &'a Ability<'a>,
+        _sim: &Simulation<'a>,
+        user: &Combatant<'a>,
+        target: &Combatant<'a>,
+    ) {
+        if user.main_hand().map_or(true, |eq| {
+            eq.weapon_type != Some(WeaponType::Sword)
+                && eq.weapon_type != Some(WeaponType::KnightSword)
+        }) {
+            return;
+        }
+
+        actions.push(Action {
+            ability,
+            range: self.range,
+            ctr: Some(self.ctr),
+            target_id: target.id(),
+        });
+    }
+    fn perform<'a>(&self, sim: &mut Simulation<'a>, user_id: CombatantId, target_id: CombatantId) {
+        let user = sim.combatant(user_id);
+        let target = sim.combatant(target_id);
+
+        if user.main_hand().map_or(true, |eq| {
+            eq.weapon_type != Some(WeaponType::Sword)
+                && eq.weapon_type != Some(WeaponType::KnightSword)
+        }) {
+            return;
+        }
+
+        // TODO: This is a weapon elemental attack sooo...
+        let mut xa = mod_3_formula_xa(user.pa() as i16, user, target, false, false);
+        if sim.roll_auto_fail() < 0.05 {
+            xa += sim.roll_inclusive(1, xa.max(1)) - 1;
+        }
+        let damage = xa * (user.main_hand().unwrap().wp as i16 + self.wp_plus);
+        sim.change_target_hp(target_id, damage, Source::Ability);
+        if sim.roll_auto_succeed() < 0.25 {
+            sim.add_condition(target_id, self.chance_to_add, Source::Ability);
+        }
+    }
+}
 
 struct BreakEquipImpl {
     base_chance: i16,
@@ -145,5 +255,45 @@ fn try_break_equip<'a>(
         sim.log_event(Event::Broke(target_id, old_equip));
     } else {
         sim.log_event(Event::AbilityMissed(user_id, target_id));
+    }
+}
+
+struct MagicBreakImpl {
+    base_chance: i16,
+    mp_percent: f32,
+}
+
+impl AbilityImpl for MagicBreakImpl {
+    fn consider<'a>(
+        &self,
+        actions: &mut Vec<Action<'a>>,
+        ability: &'a Ability<'a>,
+        _sim: &Simulation<'a>,
+        user: &Combatant<'a>,
+        target: &Combatant<'a>,
+    ) {
+        actions.push(Action {
+            ability,
+            range: user.main_hand().map_or(1, |eq| eq.range),
+            ctr: None,
+            target_id: target.id(),
+        });
+    }
+    fn perform<'a>(&self, sim: &mut Simulation<'a>, user_id: CombatantId, target_id: CombatantId) {
+        let user = sim.combatant(user_id);
+        let target = sim.combatant(target_id);
+        // TODO: Ok, I know this probably happens twice with dual wield.. bluh.
+        let xa = mod_3_formula_xa(user.pa() as i16, user, target, false, false);
+        let mut chance = (xa as f32 + self.base_chance as f32) / 100.0;
+        chance *= user.zodiac_compatibility(target);
+        if sim.do_physical_evade(user, target, Source::Ability) {
+            sim.log_event(Event::AbilityMissed(user_id, target_id));
+        } else if sim.roll_auto_succeed() < chance {
+            let mp_damage = (target.max_mp() as f32 * self.mp_percent) as i16;
+            sim.change_target_mp(target_id, mp_damage, Source::Ability);
+        } else {
+            sim.log_event(Event::AbilityMissed(user_id, target_id));
+        }
+        sim.try_countergrasp(user_id, target_id);
     }
 }
