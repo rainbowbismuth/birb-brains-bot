a015157a0d05b24a1d6d89ca2efd0497efcb5383
Emily A. Bellows <emily.a.bellows@gmail.com>
2020-07-05 19:41:39 -0400

    added reaper
    worst matches involve (out of 166):
                      A Sword: 1.9942
              A Magic Barrier: 1.9490
              C King Behemoth: 1.7849
                   A Tentacle: 1.7212
                  A Black Ink: 1.7212
                A Level Blast: 1.7212
                   C Skeleton: 1.7212
              A Odd Soundwave: 1.7212
                 A Mind Blast: 1.7212
              A Space Storage: 1.6504
              C Black Chocobo: 1.6429
               C Floating Eye: 1.5715
             A Nameless Dance: 1.4753
                     A Elixir: 1.4541
                C Pisco Demon: 1.4458
                      C Trent: 1.4458
            A Explosion Sword: 1.3691
              A Surging Sword: 1.3655
                       A Lich: 1.3633
                   A Tendrils: 1.3387
    
    total: 2000
    correct: 64.2%
    time_outs: 0.4%
    improvement: 28.5%
    log loss: 0.728428

---
+++ simulator/src/sim/actions/monster/reaper.rs
@@ -0,0 +1,89 @@
+use crate::sim::actions::monster::ChocoMeteorImpl;
+use crate::sim::actions::{Ability, AbilityImpl, Action, ALLY_OK, FOE_OK};
+use crate::sim::attack::AttackImpl;
+use crate::sim::common::{do_hp_heal, mod_2_formula_xa, mod_5_formula_xa};
+use crate::sim::{
+    AoE, Combatant, CombatantId, Condition, Element, Simulation, Source, TARGET_NOT_SELF,
+    TARGET_SELF_ONLY, TRIGGERS_HAMEDO,
+};
+
+// TODO: Line of sight.
+pub const REAPER_ABILITIES: &[Ability] = &[
+    // TODO: Knife Hand: 1 range, 0 AoE. Effect: Normal Attack; Chance to Add Undead.
+    // Thunder Soul: 3 range, 0 AoE. Element: Lightning. Effect: Damage (MA * 3).
+    Ability {
+        name: "Thunder Soul",
+        flags: ALLY_OK | FOE_OK | TARGET_NOT_SELF,
+        mp_cost: 0,
+        aoe: AoE::None,
+        implementation: &SoulAbility {
+            ma_factor: 3,
+            range: 3,
+            element: Element::Lightning,
+        },
+    },
+    // Aqua Soul: 3 range, 0 AoE. Element: Water. Effect: Damage (MA * 3).
+    Ability {
+        name: "Aqua Soul",
+        flags: ALLY_OK | FOE_OK | TARGET_NOT_SELF,
+        mp_cost: 0,
+        aoe: AoE::None,
+        implementation: &SoulAbility {
+            ma_factor: 3,
+            range: 3,
+            element: Element::Water,
+        },
+    },
+    // Ice Soul: 3 range, 0 AoE. Element: Ice. Effect: Damage (MA * 3).
+    Ability {
+        name: "Ice Soul",
+        flags: ALLY_OK | FOE_OK | TARGET_NOT_SELF,
+        mp_cost: 0,
+        aoe: AoE::None,
+        implementation: &SoulAbility {
+            ma_factor: 3,
+            range: 3,
+            element: Element::Ice,
+        },
+    },
+    // Wind Soul: 3 range, 0 AoE. Element: Wind. Effect: Damage (MA * 4).
+    Ability {
+        name: "Wind Soul",
+        flags: ALLY_OK | FOE_OK | TARGET_NOT_SELF,
+        mp_cost: 0,
+        aoe: AoE::None,
+        implementation: &SoulAbility {
+            ma_factor: 4,
+            range: 3,
+            element: Element::Wind,
+        },
+    },
+];
+
+struct SoulAbility {
+    ma_factor: i16,
+    range: u8,
+    element: Element,
+}
+
+impl AbilityImpl for SoulAbility {
+    fn consider<'a>(
+        &self,
+        actions: &mut Vec<Action<'a>>,
+        ability: &'a Ability<'a>,
+        _sim: &Simulation<'a>,
+        _user: &Combatant<'a>,
+        target: &Combatant<'a>,
+    ) {
+        actions.push(Action::new(ability, self.range, None, target.id()));
+    }
+    fn perform<'a>(&self, sim: &mut Simulation<'a>, user_id: CombatantId, target_id: CombatantId) {
+        let user = sim.combatant(user_id);
+        let target = sim.combatant(target_id);
+        if sim.do_magical_evade(user, target, Source::Ability) {
+            return;
+        }
+        let xa = mod_5_formula_xa(user.ma() as i16, user, target, self.element, true);
+        sim.change_target_hp(target_id, xa * self.ma_factor, Source::Ability);
+    }
+}
