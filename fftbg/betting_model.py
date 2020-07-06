8761637ed66915f51c8338eceba869fef6abaec4
Emily A. Bellows <emily.a.bellows@gmail.com>
2020-07-06 18:59:10 -0400

    Create betting_model.py

---
+++ fftbg/betting_model.py
@@ -0,0 +1,19 @@
+import fftbg.server
+import fftbg.bird.memory
+import logging
+import pandas
+
+LOG = logging.getLogger(__name__)
+
+
+def main():
+    fftbg.server.configure_logging('MODEL_LOG_LEVEL')
+    LOG.info('Going to compute betting model')
+    memory = fftbg.bird.memory.Memory()
+    balance_log = memory.get_balance_log(limit=1_000_000)
+
+    df = pandas.DataFrame(balance_log)
+
+    
+if __name__ == '__main__':
+    main()
