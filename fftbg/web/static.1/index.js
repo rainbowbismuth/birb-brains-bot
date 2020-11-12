89ab30db6565a948cfabb9c74332f48561ba5141
Emily A. Bellows <emily.a.bellows@gmail.com>
2020-11-11 23:53:13 -0500

    moved cyrb53 around cause fake prediction troll

--- fftbg/web/static.1/index.js
+++ fftbg/web/static.1/index.js
@@ -62,6 +62,18 @@ function format_prediction(percent) {
     return (percent * 100).toFixed(1) + '%';
 }
 
+const cyrb53 = function (str, seed = 0) {
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1 = Math.imul(h1 ^ h1 >>> 16, 2246822507) ^ Math.imul(h2 ^ h2 >>> 13, 3266489909);
+    h2 = Math.imul(h2 ^ h2 >>> 16, 2246822507) ^ Math.imul(h1 ^ h1 >>> 13, 3266489909);
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
+
 const PlacedBet = {
     view: function (vnode) {
         if (State.balance_log.length === 0) {
@@ -73,6 +85,7 @@ const PlacedBet = {
             'Betting is currently open!'
         ]);
         if (placed_bet && newest.isBefore(placed_bet.time)) {
+            // const win_prediction = (cyrb53(placed_bet.id.toString()) % 1000 / 1000);
             const win_prediction =
                 placed_bet.left_team === placed_bet.bet_on
                     ? placed_bet.left_prediction
