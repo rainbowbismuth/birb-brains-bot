2bc33b1079e72b9854e8b332e3a0284b22c92117
Emily A. Bellows <emily.a.bellows@gmail.com>
2020-11-14 01:18:32 -0500

    Add ability tests to vision testing

--- fftbg/vision/tests/runner.py
+++ fftbg/vision/tests/runner.py
@@ -10,7 +10,7 @@ READER = vision.default_character_reader()
 
 OK_DOT = colored('.', 'green')
 FAIL_DOT = colored('.', 'red')
-WRAP_AT = 90
+WRAP_AT = 100
 
 
 def add_result(img, ok, expected, reading):
@@ -44,21 +44,33 @@ def diff_string(expected, actual):
         if s[0] == ' ':
             res.append(s[-1])
         elif s[0] == '-':
-            res.append(colored(s[-1], 'green'))
+            c = s[-1]
+            if s[-1] == ' ':
+                c = '_'
+            res.append(colored(c, 'green'))
         elif s[0] == '+':
-            res.append(colored(s[-1], 'red'))
+            c = s[-1]
+            if s[-1] == ' ':
+                c = '_'
+            res.append(colored(c, 'red'))
     return ''.join(res)
 
 
 def report_results():
-    print()
+    print('\n')
+    h = ["file", "finder", "expected", "actual", "diff"]
+    print(colored(f'{h[0]:<38} {h[1]:<10} {h[2]:<30} {h[3]:<30} {h[4]}', 'cyan'))
+    print(f'{"-"*37}  {"-"*9}  {"-"*29}  {"-"*29}  {"-"*29}')
     for result in TEST_RESULTS:
         if not result["ok"]:
             expected = result["expected"]
             actual = result["reading"].value
             name = result["reading"].name
             diff = diff_string(expected, actual)
-            print(f'{result["img"]:<30} {name:<10} {expected:<30} {actual:<30} {diff}')
+            print(f'{result["img"]:<38} {name:<10} {expected:<30} {actual:<30} {diff}')
+
+    ok_count = len([r for r in TEST_RESULTS if r["ok"]])
+    print(f'\n{ok_count} OK / {len(TEST_RESULTS)} TOTAL')
 
 
 run_text()
