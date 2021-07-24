f168ec06c5c5cc8d41589437c6f91f0d97289167
Emily Bellows <emily.a.bellows@hey.com>
2021-07-24 12:49:53 -0400

    reformat ganesha code w/ black

--- ganesha/fft/map/texture.py
+++ ganesha/fft/map/texture.py
@@ -7,7 +7,7 @@ class Texture(object):
     def read(self, files):
         for file_path in files:
             self.file_path = file_path
-            self.file = open(self.file_path, 'rb')
+            self.file = open(self.file_path, "rb")
             self.data = self.file.read()
             break
         # print 'tex', self.file_path
@@ -15,7 +15,7 @@ class Texture(object):
             self.file.close()
 
     def write(self, data):
-        self.file = open(self.file_path, 'wb')
+        self.file = open(self.file_path, "wb")
         # print 'Writing', self.file_path
         self.file.write(data)
         self.file.close()
